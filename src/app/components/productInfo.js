14f5bdcdcef11571a56cc2cb72955b6fcd094ee3
mik91 <41644495+mik91@users.noreply.github.com>
2024-03-02 15:26:02 -0500

    Improving product page

--- src/app/components/productInfo.js
+++ src/app/components/productInfo.js
@@ -3,15 +3,21 @@ import styles from './page.module.css';
 
 const ProductInfo = ({ product }) => {
     return (
-        <div id='container'>
-            <h2>{product.name}</h2>
+        <div id='productContainer'>
+            <div id={styles.productImage}>
+                <img id={styles.img} src={product.imageUrl} alt={product.name} />
+            </div>
+            <div id={styles.productDetails}>
+                <h1>{product.name}</h1>
+                <p>{product.brand} | {product.model}</p>
+            </div>
             <p>{product.description}</p>
             <p id={styles.price}>${product.price}</p>
-            <img id={styles.img} src={product.imageUrl} alt={product.name} />
             
             <button id={styles.addToCart}>Add to Cart</button>
         </div>
+        
+
     );
 };
-
 export default ProductInfo;
--- src/app/product/page.js
+++ src/app/product/page.js
@@ -5,6 +5,8 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import ProductInfo from '../components/productInfo';
+import Image from 'next/image';
+import logo from '../../../public/images/logo.png';
 
 export default function Product() {
     const product = {
@@ -12,17 +14,22 @@ export default function Product() {
         name: "Amazing Widget",
         description: "This widget is amazing. It does everything you need and more.",
         price: 49.99,
-        imageUrl: "https://example.com/image.jpg"
+        imageUrl: "https://www.patagonia.ca/dw/image/v2/BDJB_PRD/on/demandware.static/-/Sites-patagonia-master/default/dw8de7e495/images/hi-res/20655_BSNG.jpg?sw=512&sh=512&sfrm=png&q=95&bgcolor=f5f5f5",
+        rating: 4.5,
+        reviews: 10,
+        brand: "Patagonia",
+        model: "881046-0010"
     };
 
     return (
         <Container>
         {/* Header */}
         <Row className='justify-content-md-center'>
-            <Col md="auto">
-            <h1>Patagonia</h1>
-            </Col>
-        </Row>
+        <Col xl='12' md='auto' className='justify-content-md-center'>
+          {/* Display the image called logo.png */}
+          <Image src={logo} alt='logo' height={80} />
+        </Col>
+      </Row>
     
         {/* Content */}
         <Row>
