009d8c78d2b903f63cbad1a1682d6cbd8be2d7e8
mik91 <41644495+mik91@users.noreply.github.com>
2024-03-02 16:03:49 -0500

    Improving product page

--- src/app/components/productInfo.js
+++ src/app/components/productInfo.js
@@ -3,18 +3,24 @@ import styles from './page.module.css';
 
 const ProductInfo = ({ product }) => {
     return (
-        <div id='productContainer'>
+        <div id={styles.productContainer}>
             <div id={styles.productImage}>
                 <img id={styles.img} src={product.imageUrl} alt={product.name} />
             </div>
             <div id={styles.productDetails}>
                 <h1>{product.name}</h1>
                 <p>{product.brand} | {product.model}</p>
+                <div id={styles.rating}>
+                    <p>Rating: {product.rating} ({product.reviews} reviews)</p>
+                </div>
+                <p id={styles.price}>${product.price}</p>
+                <button id={styles.addToCart}>Add to Cart</button>
+
             </div>
-            <p>{product.description}</p>
-            <p id={styles.price}>${product.price}</p>
-            
-            <button id={styles.addToCart}>Add to Cart</button>
+            <div id={styles.productDescription}>
+                <h3>Description</h3>
+                <p>{product.description}</p>
+            </div>
         </div>
         
 
--- src/app/product/page.js
+++ src/app/product/page.js
@@ -6,15 +6,14 @@ import Container from 'react-bootstrap/Container';
 import ProductInfo from '../components/productInfo';
 import Image from 'next/image';
 import logo from '../../../public/images/logo.png';
-import Image from 'next/image';
-import logo from '../../../public/images/logo.png';
+
 
 export default function Product() {
     
     const product = {
         id: 1,
         name: "Amazing Widget",
-        description: "This widget is amazing. It does everything you need and more.",
+        description: "Whether you’re seeking boulders and alpine views or stalking rainbow trout, this low-profile, waterproof jacket will keep you dry and provide rain protection from the fork in the trail to the places less traveled, and back again. The 3-layer shell meets our H2No® Performance Standard for exceptional waterproof/breathable protection and the fabrics, membrane and durable water repellent (DWR) finish are made without perfluorinated chemicals (PFCs/PFAS). Made in a Fair Trade Certified™ factory.",
         price: 49.99,
         imageUrl: "https://www.patagonia.ca/dw/image/v2/BDJB_PRD/on/demandware.static/-/Sites-patagonia-master/default/dw8de7e495/images/hi-res/20655_BSNG.jpg?sw=512&sh=512&sfrm=png&q=95&bgcolor=f5f5f5",
         rating: 4.5,
