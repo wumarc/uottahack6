adc1b4c6f8c7d0c11fb4a81b9b3bab4432ae8970
Marc WU <32423253+wumarc@users.noreply.github.com>
2024-03-02 11:48:45 -0500

    for Mik ❤️

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,32 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import React from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
 
 export default function Home() {
   return (
-    <main>
-      <div>
-        <h1>Welcome to my website</h1>
-      </div>
-    </main>
+    <Container>
+      {/* Header */}
+      <Row className='justify-content-md-center'>
+        <Col md="auto">
+          <h1>Patagonia</h1>
+        </Col>
+      </Row>
+
+      {/* Content */}
+      <Row>
+        <Col>
+          <h1>Add Items Here</h1>
+        </Col>
+      </Row>
+
+      {/* Footer */}
+      <Row>
+        <Col>
+          <h1>Footer</h1>
+        </Col>
+      </Row>
+
+    </Container>
   );
 }
