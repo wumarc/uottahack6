e64d13af4822ade17a9a202f68750070fdd38606
Marc WU <32423253+wumarc@users.noreply.github.com>
2024-03-02 11:58:25 -0500

    change

--- src/app/page.js
+++ src/app/page.js
@@ -8,16 +8,23 @@ export default function Home() {
     <Container>
       {/* Header */}
       <Row className='justify-content-md-center'>
-        <Col md="auto">
+        <Col lg='12'>
           <h1>Patagonia</h1>
         </Col>
       </Row>
 
       {/* Content */}
       <Row>
+        
         <Col>
-          <h1>Add Items Here</h1>
+          <h2>Jackets</h2>
         </Col>
+
+        <Col>
+          <h2>Hoodies</h2>
+        </Col>
+
+
       </Row>
 
       {/* Footer */}
