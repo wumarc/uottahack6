d8071587a89855d6763fc2ca696914b9922fd433
Marc WU <32423253+wumarc@users.noreply.github.com>
2024-03-02 13:06:43 -0500

    added logo

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,20 @@
+'use client'
 import React from 'react';
+import Image from 'next/image';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
+import logo from '../../public/images/logo.png';
 
 export default function Home() {
+  
   return (
     <Container>
       {/* Header */}
       <Row className='justify-content-md-center'>
-        <Col lg='12'>
-          <h1>Patagonia</h1>
+        <Col lg='12' className='justify-content-md-center'>
+          {/* Display the image called logo.png */}
+          <Image src={logo} alt='logo' />
         </Col>
       </Row>
 
