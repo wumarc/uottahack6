3c07b520a656cb2924c501db21fb407cac4a6aef
Marc WU <32423253+wumarc@users.noreply.github.com>
2024-03-02 19:28:09 -0500

    added metrics

--- src/app/components/productInfo.js
+++ src/app/components/productInfo.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from './page.module.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ProgressBar from 'react-bootstrap/ProgressBar';
 
 const ProductInfo = ({ product }) => {
     return (
@@ -15,6 +17,23 @@ const ProductInfo = ({ product }) => {
                 </div>
                 <p id={styles.price}>${product.price}</p>
                 <button id={styles.addToCart}>Add to Cart</button>
+
+                {/* Metrics */}
+                <h3>Carbon Footprint Breakdown</h3>
+
+                <ProgressBar animated variant='primary' now={60} label={`${60}%`} />
+                <br/>
+
+                <ProgressBar animated variant='info' now={30} label={`${30}%`} />
+                <br/>
+
+                <ProgressBar animated variant='warning' now={80} label={`${80}%`} />
+                <br/>
+
+                <ProgressBar animated variant='success' now={20} label={`${20}%`} />
+                <br/>
+
+                <p>Total Carbon Footprint</p>
 
             </div>
             <div id={styles.productDescription}>
