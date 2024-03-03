4a80957545c2716f154306658001aff588ed4e7a
Marc WU <32423253+wumarc@users.noreply.github.com>
2024-03-02 23:29:25 -0500

    added chart

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -7,7 +7,8 @@ import Image from 'next/image';
 import logo from '../../../public/images/logo.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { LineChart } from '@mui/x-charts/LineChart';
-
+import { PieChart } from '@mui/x-charts/PieChart';
+import { BarChart } from '@mui/x-charts/BarChart';
 
 export default function Dashboard({}) {
 
@@ -35,11 +36,28 @@ export default function Dashboard({}) {
             />
 
             {/* Chart 2 */}
-
+            <PieChart
+                series={[
+                    {
+                    data: [
+                        { id: 0, value: 10, label: 'series A' },
+                        { id: 1, value: 15, label: 'series B' },
+                        { id: 2, value: 20, label: 'series C' },
+                    ],
+                    },
+                ]}
+                width={400}
+                height={200}
+            />
 
 
             {/* Chart 3 */}
-
+            <BarChart
+                xAxis={[{ scaleType: 'band', data: ['group A', 'group B', 'group C'] }]}
+                series={[{ data: [4, 3, 5] }, { data: [1, 6, 3] }, { data: [2, 5, 6] }]}
+                width={500}
+                height={300}
+            />
 
 
         </Container>
